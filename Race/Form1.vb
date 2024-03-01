27602878e0a648e6d67c5719e8fdbe58c9dcb1c3
Dré <43050254+jackcooperusesvim@users.noreply.github.com>
2024-03-01 13:27:12 -0500

    Hopefully this whole array thing works...

--- Race/Form1.vb
+++ Race/Form1.vb
@@ -1,4 +1,11 @@
-﻿Public Class Form1
+Public Class Form1
+    Function Throw_Error()
+        Error_Label.Text = "Please use only proper integers for Race "
+    End Function
+    Function Bad_Numbers()
+        Error_Label.Text = "Enter 1, 2 and 3 only for Race "
+    End Function
+
     Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
         R1B1.Text = ""
         R1B2.Text = ""
@@ -14,10 +21,60 @@
     End Sub
 
     Private Sub Calc_Btn_Click(sender As Object, e As EventArgs) Handles Calc_Btn.Click
-        Try
-            Convert.ToInt32(R1B1)+Convert.ToInt32(R1B1)+Convert.ToInt32(R1B1)+
-        Catch ex As Exception
+        Dim InArr as TextBox = {{R1B1,R1B2,R1B3},{R2B1,R2B2,R2B3},{R3B1,R3B2,R3B3}}
+        Dim OutArr as TextBox = {{TotalB1,TotalB2,TotalB3},{RankB1,RankB2,RankB3}}
 
-        End Try
+        Dim IntArr as TextBox
+
+        Error_Label.Text = ""
+
+        'Check For Bad Input
+        For Race = 0 To 2
+            Try:
+                For Boat = 0 To 2
+                    IntArr(Race,Boat) = Convert.ToInt32(InArr(Race,Boat).Text)
+                Next
+            Catch ex As Exception
+                Bad_Numbers()
+                Error_Label.Text = Error_Label.Text+Convert.ToString(Race+1)
+                Exit For
+            End Try
+            Next
+        Next
+
+        'Check for a total of 6
+        If Error_Label.Text == ""
+            Dim Check As Integer 
+            For Race = 0 To 2
+                Check = 0
+                For Boat = 0 To 2
+                    Check = Check + IntArr(Race,Boat)
+                Next
+                If Check != 6
+                    Bad_Numbers()
+                    Error_Label.Text = Error_Label.Text+Convert.ToString(Race+1)
+                    Exit For
+                End If
+            Next
+        End If
+
+        'Calculate and Update Total
+        If Error_Label.Text == ""
+            OutIntArr As Integer = {0,0,0}
+            For Boat = 0 To 2
+                'Calculate
+                OutIntArr(Boat) = 0
+                For Race = 0 To 2
+                    OutIntArr(Boat) = OutIntArr(Boat) + IntArr(Boat,Race)
+                Next
+                'Update
+                OutArr(0,Boat).Text = Convert.ToString(OutIntArr)
+            Next
+       End If
+    End Sub
+
+
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles Close.Click
+
     End Sub
 End Class
