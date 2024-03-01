2e0250565558559f83b787931a5bcb8324e5ac14
A00377359 <A00377359@MCC-SAN-STD-051>
2024-03-01 11:23:08 -0500

    Add project files.

---
+++ Race/Form1.Designer.vb
@@ -0,0 +1,381 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.R1B1 = New System.Windows.Forms.TextBox()
+        Me.R1B2 = New System.Windows.Forms.TextBox()
+        Me.R1B3 = New System.Windows.Forms.TextBox()
+        Me.R3B3 = New System.Windows.Forms.TextBox()
+        Me.R3B2 = New System.Windows.Forms.TextBox()
+        Me.R3B1 = New System.Windows.Forms.TextBox()
+        Me.R2B3 = New System.Windows.Forms.TextBox()
+        Me.R2B2 = New System.Windows.Forms.TextBox()
+        Me.R2B1 = New System.Windows.Forms.TextBox()
+        Me.R4B3 = New System.Windows.Forms.TextBox()
+        Me.R4B2 = New System.Windows.Forms.TextBox()
+        Me.R4B1 = New System.Windows.Forms.TextBox()
+        Me.TotalB3 = New System.Windows.Forms.TextBox()
+        Me.TotalB2 = New System.Windows.Forms.TextBox()
+        Me.TotalB1 = New System.Windows.Forms.TextBox()
+        Me.RankB3 = New System.Windows.Forms.TextBox()
+        Me.RankB2 = New System.Windows.Forms.TextBox()
+        Me.RankB1 = New System.Windows.Forms.TextBox()
+        Me.Error_Label = New System.Windows.Forms.Label()
+        Me.Calc_Btn = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Close = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Boat #1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Boat #2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(42, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Boat #3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(101, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Race 1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(160, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Race 2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(233, 27)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Race 3"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(309, 27)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Race 4"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(384, 27)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(31, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Total"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(442, 27)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(33, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Rank"
+        '
+        'R1B1
+        '
+        Me.R1B1.Location = New System.Drawing.Point(104, 56)
+        Me.R1B1.Name = "R1B1"
+        Me.R1B1.Size = New System.Drawing.Size(39, 20)
+        Me.R1B1.TabIndex = 9
+        '
+        'R1B2
+        '
+        Me.R1B2.Location = New System.Drawing.Point(104, 96)
+        Me.R1B2.Name = "R1B2"
+        Me.R1B2.Size = New System.Drawing.Size(39, 20)
+        Me.R1B2.TabIndex = 10
+        '
+        'R1B3
+        '
+        Me.R1B3.Location = New System.Drawing.Point(104, 134)
+        Me.R1B3.Name = "R1B3"
+        Me.R1B3.Size = New System.Drawing.Size(39, 20)
+        Me.R1B3.TabIndex = 11
+        '
+        'R3B3
+        '
+        Me.R3B3.Location = New System.Drawing.Point(236, 133)
+        Me.R3B3.Name = "R3B3"
+        Me.R3B3.Size = New System.Drawing.Size(39, 20)
+        Me.R3B3.TabIndex = 14
+        '
+        'R3B2
+        '
+        Me.R3B2.Location = New System.Drawing.Point(236, 96)
+        Me.R3B2.Name = "R3B2"
+        Me.R3B2.Size = New System.Drawing.Size(39, 20)
+        Me.R3B2.TabIndex = 13
+        '
+        'R3B1
+        '
+        Me.R3B1.Location = New System.Drawing.Point(236, 56)
+        Me.R3B1.Name = "R3B1"
+        Me.R3B1.Size = New System.Drawing.Size(39, 20)
+        Me.R3B1.TabIndex = 12
+        '
+        'R2B3
+        '
+        Me.R2B3.Location = New System.Drawing.Point(163, 133)
+        Me.R2B3.Name = "R2B3"
+        Me.R2B3.Size = New System.Drawing.Size(39, 20)
+        Me.R2B3.TabIndex = 17
+        '
+        'R2B2
+        '
+        Me.R2B2.Location = New System.Drawing.Point(163, 96)
+        Me.R2B2.Name = "R2B2"
+        Me.R2B2.Size = New System.Drawing.Size(39, 20)
+        Me.R2B2.TabIndex = 16
+        '
+        'R2B1
+        '
+        Me.R2B1.Location = New System.Drawing.Point(163, 56)
+        Me.R2B1.Name = "R2B1"
+        Me.R2B1.Size = New System.Drawing.Size(39, 20)
+        Me.R2B1.TabIndex = 15
+        '
+        'R4B3
+        '
+        Me.R4B3.Location = New System.Drawing.Point(312, 133)
+        Me.R4B3.Name = "R4B3"
+        Me.R4B3.Size = New System.Drawing.Size(39, 20)
+        Me.R4B3.TabIndex = 20
+        '
+        'R4B2
+        '
+        Me.R4B2.Location = New System.Drawing.Point(312, 96)
+        Me.R4B2.Name = "R4B2"
+        Me.R4B2.Size = New System.Drawing.Size(39, 20)
+        Me.R4B2.TabIndex = 19
+        '
+        'R4B1
+        '
+        Me.R4B1.Location = New System.Drawing.Point(312, 56)
+        Me.R4B1.Name = "R4B1"
+        Me.R4B1.Size = New System.Drawing.Size(39, 20)
+        Me.R4B1.TabIndex = 18
+        '
+        'TotalB3
+        '
+        Me.TotalB3.Location = New System.Drawing.Point(376, 133)
+        Me.TotalB3.Name = "TotalB3"
+        Me.TotalB3.ReadOnly = True
+        Me.TotalB3.Size = New System.Drawing.Size(39, 20)
+        Me.TotalB3.TabIndex = 23
+        '
+        'TotalB2
+        '
+        Me.TotalB2.Location = New System.Drawing.Point(376, 96)
+        Me.TotalB2.Name = "TotalB2"
+        Me.TotalB2.ReadOnly = True
+        Me.TotalB2.Size = New System.Drawing.Size(39, 20)
+        Me.TotalB2.TabIndex = 22
+        '
+        'TotalB1
+        '
+        Me.TotalB1.Location = New System.Drawing.Point(376, 56)
+        Me.TotalB1.Name = "TotalB1"
+        Me.TotalB1.ReadOnly = True
+        Me.TotalB1.Size = New System.Drawing.Size(39, 20)
+        Me.TotalB1.TabIndex = 21
+        '
+        'RankB3
+        '
+        Me.RankB3.Location = New System.Drawing.Point(436, 133)
+        Me.RankB3.Name = "RankB3"
+        Me.RankB3.ReadOnly = True
+        Me.RankB3.Size = New System.Drawing.Size(39, 20)
+        Me.RankB3.TabIndex = 26
+        '
+        'RankB2
+        '
+        Me.RankB2.Location = New System.Drawing.Point(436, 96)
+        Me.RankB2.Name = "RankB2"
+        Me.RankB2.ReadOnly = True
+        Me.RankB2.Size = New System.Drawing.Size(39, 20)
+        Me.RankB2.TabIndex = 25
+        '
+        'RankB1
+        '
+        Me.RankB1.Location = New System.Drawing.Point(436, 56)
+        Me.RankB1.Name = "RankB1"
+        Me.RankB1.ReadOnly = True
+        Me.RankB1.Size = New System.Drawing.Size(39, 20)
+        Me.RankB1.TabIndex = 24
+        '
+        'Error_Label
+        '
+        Me.Error_Label.AutoSize = True
+        Me.Error_Label.Location = New System.Drawing.Point(12, 210)
+        Me.Error_Label.Name = "Error_Label"
+        Me.Error_Label.Size = New System.Drawing.Size(61, 13)
+        Me.Error_Label.TabIndex = 27
+        Me.Error_Label.Text = "Error_Label"
+        '
+        'Calc_Btn
+        '
+        Me.Calc_Btn.Location = New System.Drawing.Point(76, 179)
+        Me.Calc_Btn.Name = "Calc_Btn"
+        Me.Calc_Btn.Size = New System.Drawing.Size(126, 23)
+        Me.Calc_Btn.TabIndex = 28
+        Me.Calc_Btn.Text = "Calculate Rankings"
+        Me.Calc_Btn.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Location = New System.Drawing.Point(257, 179)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(75, 23)
+        Me.Clear.TabIndex = 29
+        Me.Clear.Text = "Clear"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'Close
+        '
+        Me.Close.Location = New System.Drawing.Point(376, 179)
+        Me.Close.Name = "Close"
+        Me.Close.Size = New System.Drawing.Size(75, 23)
+        Me.Close.TabIndex = 30
+        Me.Close.Text = "Close"
+        Me.Close.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 232)
+        Me.Controls.Add(Me.Close)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Calc_Btn)
+        Me.Controls.Add(Me.Error_Label)
+        Me.Controls.Add(Me.RankB3)
+        Me.Controls.Add(Me.RankB2)
+        Me.Controls.Add(Me.RankB1)
+        Me.Controls.Add(Me.TotalB3)
+        Me.Controls.Add(Me.TotalB2)
+        Me.Controls.Add(Me.TotalB1)
+        Me.Controls.Add(Me.R4B3)
+        Me.Controls.Add(Me.R4B2)
+        Me.Controls.Add(Me.R4B1)
+        Me.Controls.Add(Me.R2B3)
+        Me.Controls.Add(Me.R2B2)
+        Me.Controls.Add(Me.R2B1)
+        Me.Controls.Add(Me.R3B3)
+        Me.Controls.Add(Me.R3B2)
+        Me.Controls.Add(Me.R3B1)
+        Me.Controls.Add(Me.R1B3)
+        Me.Controls.Add(Me.R1B2)
+        Me.Controls.Add(Me.R1B1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents R1B1 As TextBox
+    Friend WithEvents R1B2 As TextBox
+    Friend WithEvents R1B3 As TextBox
+    Friend WithEvents R3B3 As TextBox
+    Friend WithEvents R3B2 As TextBox
+    Friend WithEvents R3B1 As TextBox
+    Friend WithEvents R2B3 As TextBox
+    Friend WithEvents R2B2 As TextBox
+    Friend WithEvents R2B1 As TextBox
+    Friend WithEvents R4B3 As TextBox
+    Friend WithEvents R4B2 As TextBox
+    Friend WithEvents R4B1 As TextBox
+    Friend WithEvents TotalB3 As TextBox
+    Friend WithEvents TotalB2 As TextBox
+    Friend WithEvents TotalB1 As TextBox
+    Friend WithEvents RankB3 As TextBox
+    Friend WithEvents RankB2 As TextBox
+    Friend WithEvents RankB1 As TextBox
+    Friend WithEvents Error_Label As Label
+    Friend WithEvents Calc_Btn As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Close As Button
+End Class
---
+++ Race/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        R1B1.Text = ""
+        R1B2.Text = ""
+        R1B3.Text = ""
+
+        R2B1.Text = ""
+        R2B2.Text = ""
+        R2B3.Text = ""
+
+        R3B1.Text = ""
+        R3B2.Text = ""
+        R3B3.Text = ""
+    End Sub
+
+    Private Sub Calc_Btn_Click(sender As Object, e As EventArgs) Handles Calc_Btn.Click
+        Try
+            Convert.ToInt32(R1B1)+Convert.ToInt32(R1B1)+Convert.ToInt32(R1B1)+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
